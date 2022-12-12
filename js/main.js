aba13af0cba0513e3c13d2d81a0e790d6edc56a9
mstrdav <colin.geindre@gmail.com>
2022-12-12 10:12:43 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let quote, author = "";
+
+// fisrt, we call the quote API to get the quote data
+let quoteAPIURL = "https://api.quotable.io/random";
+fetch(quoteAPIURL)
+.catch(function(error) {
+    console.log(error);
+})
+.then(function(response) {
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+    quote = data.content;
+    author = data.author;
+    document.querySelector('#author').innerHTML = author;
+
+    // to upper case
+    quote = quote.toUpperCase();
+
+    // crypt the quote
+    let encryptedQuote = cryptQuote(quote);
+    document.querySelector('#quote').innerHTML = encryptedQuote;
+});
+
+// crypt quote
+function cryptQuote(quote) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let shuffledAlphabet = alphabet.split('').sort(function(){return 0.5-Math.random()}).join('');
+    let encryptedQuote = "";
+    for (let i = 0; i < quote.length; i++) {
+        let letter = quote[i];
+        let index = alphabet.indexOf(letter);
+        if (index >= 0) {
+            encryptedQuote += shuffledAlphabet[index];
+        } else {
+            encryptedQuote += letter;
+        }
+    }
+
+    return encryptedQuote;
+}
