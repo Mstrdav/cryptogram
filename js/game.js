706d7e65217a8e3dcafef3b98a935fa518dba51c
mstrdav <colin.geindre@gmail.com>
2023-01-02 12:40:17 +0100

    auto uppercase, block non alphabetic entries

--- js/game.js
+++ js/game.js
@@ -5,10 +5,9 @@ const loadListeners = () => {
   document.querySelectorAll("input").forEach((input) => {
     input.addEventListener("focus", function () {
       // scroll to make this input visible, even with keyboard
-      this.scrollIntoView({ behavior: "auto", block: "start" });
+      // this.scrollIntoView({ behavior: "auto", block: "start" });
       // scroll back a little
-      window.scrollBy(0, -100);
-      
+      // window.scrollBy(0, -100);
 
       let letter = this.getAttribute("data-index");
       document
@@ -22,67 +21,83 @@ const loadListeners = () => {
 
     // on input change, change all others
     input.addEventListener("input", function () {
-      let letter = this.getAttribute("data-index");
-      document
-        .querySelectorAll(`input[data-index="${letter}"]`)
-        .forEach((input) => {
-          // change the value of the input
-          input.value = this.value;
-        });
-
-      // go to the next empty input only if the current input is not empty
-      // struct : (.word > (span > input + span) * 5)*20
-      
-      // find all inputs
-      let inputs = Array.from(document.querySelectorAll("input"));
-
-      // find the index of the current input
-      let index = inputs.indexOf(this);
-
-      // reorder the inputs : start from index and loop
-      let reorderedInputs = inputs.slice(index, inputs.length - 1).concat(inputs.slice(0, index - 1));
-
-      // find the next empty input
-      index = 0;
-      let nextInput = reorderedInputs[0];
-      while (nextInput && nextInput.value != "") {
-        nextInput = reorderedInputs[++index];
-      }
-
-      // if there is a next input, focus on it
-      if (nextInput) {
-        nextInput.focus();
+      // if the value is not a letter, remove it
+      if (alphabet.indexOf(this.value.toUpperCase()) < 0) {
+        this.value = "";
+        // shake the input
+        this.classList.add("shake");
+        setTimeout(() => {
+          this.classList.remove("shake");
+        }, 500);
       } else {
-        // check if the user has the right answer
-        // we get the hash from the local storage
-        let hash = localStorage.getItem("hash");
+        this.value = this.value.toUpperCase();
 
-        // we get the value of the inputs
-        let value = "";
-        inputs.forEach((input) => {
-          value += input.value;
-        });
+        let letter = this.getAttribute("data-index");
+        document
+          .querySelectorAll(`input[data-index="${letter}"]`)
+          .forEach((input) => {
+            // change the value of the input
+            input.value = this.value;
+          });
 
-        if (value.split("").sort().join("") == hash) {
-          // the user has the right answer
-          // we show the modal
-          score = Date.now() - score;
-          // make score human readable
-          score = Math.floor(score / 1000);
-          // display the score, with minutes and seconds
-          document.querySelector("#modal-score").innerHTML = `${Math.floor(score / 60)} min, ${score % 60} s`;
-          document.querySelector("#modal").classList.add("active");
-        } else {
-          // the user has the wrong answer
-          // we shake the screen
-          document.querySelector("#quote").classList.add("shake");
-          setTimeout(() => {
-            document.querySelector("#quote").classList.remove("shake");
-          }, 1000);
+        // go to the next empty input only if the current input is not empty
+        // struct : (.word > (span > input + span) * 5)*20
+
+        // find all inputs
+        let inputs = Array.from(document.querySelectorAll("input"));
+
+        // find the index of the current input
+        let index = inputs.indexOf(this);
+
+        // reorder the inputs : start from index and loop
+        let reorderedInputs = inputs
+          .slice(index, inputs.length - 1)
+          .concat(inputs.slice(0, index - 1));
+
+        // find the next empty input
+        index = 0;
+        let nextInput = reorderedInputs[0];
+        while (nextInput && nextInput.value != "") {
+          nextInput = reorderedInputs[++index];
         }
 
-        //unfocus
-        input.blur();
+        // if there is a next input, focus on it
+        if (nextInput) {
+          nextInput.focus();
+        } else {
+          // check if the user has the right answer
+          // we get the hash from the local storage
+          let hash = localStorage.getItem("hash");
+
+          // we get the value of the inputs
+          let value = "";
+          inputs.forEach((input) => {
+            value += input.value;
+          });
+
+          if (value.split("").sort().join("") == hash) {
+            // the user has the right answer
+            // we show the modal
+            score = Date.now() - score;
+            // make score human readable
+            score = Math.floor(score / 1000);
+            // display the score, with minutes and seconds
+            document.querySelector("#modal-score").innerHTML = `${Math.floor(
+              score / 60
+            )} min, ${score % 60} s`;
+            document.querySelector("#modal").classList.add("active");
+          } else {
+            // the user has the wrong answer
+            // we shake the screen
+            document.querySelector("#quote").classList.add("shake");
+            setTimeout(() => {
+              document.querySelector("#quote").classList.remove("shake");
+            }, 1000);
+          }
+
+          //unfocus
+          input.blur();
+        }
       }
     });
 
